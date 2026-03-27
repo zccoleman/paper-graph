fd0af4d592a2e2be54a9a01aa61d720db81f37ac
Zachary Coleman <Coleman.ZacharyC@gmail.com>
2026-03-27 18:09:26 -0400

    Rename test

---
+++ test/test_environ.py
@@ -0,0 +1,11 @@
+
+def test_import():
+    import paper_graph
+
+def test_dotenv():
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    test = os.getenv("TEST_ENV")
+    assert test=='True'
