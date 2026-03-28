62ea02b9a3f0d03036de152da7ef4e381640077e
Zachary Coleman <Coleman.ZacharyC@gmail.com>
2026-03-28 16:58:59 -0400

    Added API credit checking

--- src/paper_graph/openalex.py
+++ src/paper_graph/openalex.py
@@ -2,6 +2,8 @@ import os
 import requests
 import time
 
+from typing import Optional
+
 def fetch_with_retry(url, max_retries=5, return_full_response=False):
     for attempt in range(max_retries):
         try:
@@ -35,11 +37,35 @@ def fetch_with_retry(url, max_retries=5, return_full_response=False):
 
     raise Exception(f"Failed after {max_retries} retries")
 
-def lookup_work(id, suppress_errors=False, fields:list[str]=[], api_key=None, ):
-    if not isinstance(id, str):
-        raise TypeError('Work ID must be a string.')
+def check_api_key(api_key):
     if api_key is None:
         api_key = os.getenv('OPENALEX_KEY')
+    if not isinstance(api_key, str):
+        raise ValueError('Invalid API key type')
+    return api_key
+
+def _api_credit_check(api_key:Optional[str]=None):
+    api_key = check_api_key(api_key)
+    request = f"https://api.openalex.org/rate-limit?api_key={api_key}"
+    result = fetch_with_retry(request)
+    return result
+
+def api_credit_check(api_key:Optional[str]=None):
+    result = _api_credit_check(api_key)
+    rate_limit = result['rate_limit']
+    return dict(
+        fraction_credits_used = rate_limit['credits_used'] / rate_limit['credits_limit'],
+        fraction_credits_remaining = rate_limit['credits_remaining'] / rate_limit['credits_limit'],
+        list_queries_remaining = rate_limit['credits_remaining'] // rate_limit['credit_costs']['list'],
+        seconds_until_reset = rate_limit['resets_in_seconds'],
+        hours_until_reset = rate_limit['resets_in_seconds'] / 3600.,
+    )
+
+
+def lookup_work(id, suppress_errors=False, fields:list[str]=[], api_key:Optional[str]=None, ):
+    if not isinstance(id, str):
+        raise TypeError('Work ID must be a string.')
+    api_key = check_api_key(api_key)
     
     s = f'https://api.openalex.org/works/{id}?api_key={api_key}'
     if fields:
