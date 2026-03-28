7136696d2cf12b0b7e924b6f5fa43e0c3fbe31eb
Zachary Coleman <Coleman.ZacharyC@gmail.com>
2026-03-28 16:40:38 -0400

    add test for basic html fetch

--- test/test_works.py
+++ test/test_works.py
@@ -1,7 +1,18 @@
+import os
+
 import paper_graph
 
+def test_api_key():
+    from paper_graph.openalex import fetch_with_retry
+    api_key = os.getenv('OPENALEX_KEY')
+    id = 'doi:10.1088/1361-6455/ac5efa'
+    request = f'https://api.openalex.org/works/{id}?api_key={api_key}'
+    result = fetch_with_retry(request)
+    assert isinstance(result, dict)
+
+
 def test_get_work():
-    from paper_graph.works import lookup_work
+    from paper_graph.openalex import lookup_work
 
     work_id = 'https://doi.org/10.1038/s42254-019-0054-2' ## nominal test work
     result = lookup_work(work_id)
